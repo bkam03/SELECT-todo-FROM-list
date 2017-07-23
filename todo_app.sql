f185ce21a2f8e5e7f0405d625d4b4cc8b5f51f99
BK <bkam03@yahoo.com>
2017-07-22 14:55:20 -1000

    creates table tasks, added connection switch so later runs can drop database

--- todo_app.sql
+++ todo_app.sql
@@ -10,3 +10,25 @@ CREATE USER michael WITH ENCRYPTED PASSWORD 'stonebreaker';
 
 DROP DATABASE IF EXISTS todo_app;
 CREATE DATABASE todo_app;
+
+/*
+Connect to the newly created database
+Write a query to create a table named tasks using the Initial columns detailed below
+Define column id as the table's primary key
+*/
+\c todo_app;
+
+CREATE TABLE tasks (
+  id SERIAL NOT NULL PRIMARY KEY,
+  title VARCHAR(255) NOT NULL,
+  description TEXT DEFAULT NULL,
+  created_at TIMESTAMP without time zone NOT NULL DEFAULT NOW(),
+  updated_at TIMESTAMP WITH TIME ZONE DEFAULT NULL,
+  completed BOOLEAN NOT NULL DEFAULT FALSE
+);
+
+/*
+8
+*/
+
+\c bk;
